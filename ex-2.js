74df30bdda02da26ec46614859bb847af240f237
Pond <pondwachi.th@gmail.com>
2024-07-03 12:14:48 +0700

    feat: nested loop

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
-}
+function printStar(n) {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+     let row = "";
+     for (let o = 1; o <= i; o++) {
+         row += "*" + "\t";
+     }
+     result+= row + "\n"
+    }
+    return result
+ }
+
+ console.log(printStar(5));
+
